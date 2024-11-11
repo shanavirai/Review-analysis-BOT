cdd2b881ead214cc4c768bb7abf0da138dd89bfe
shanavirai <shanavyarai@gmail.com>
2024-11-11 07:52:07 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Install the spaCy model required for the app
-python -m spacy download en_core_web_sm
+pip install spacy==3.5.0  # Specify the spaCy version compatible with your environment
+python -m spacy download en_core_web_sm  # Install the specific spaCy model
